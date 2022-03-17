a6718bfc83fb0983c191d4b204870b80a53c1925
Trey Husko <trey.husko@gmail.com>
2022-03-17 15:17:43 -0700

    Played around with theme

--- app/src/main/java/com/example/finalapp/SettingsFragment.kt
+++ app/src/main/java/com/example/finalapp/SettingsFragment.kt
@@ -1,11 +1,15 @@
 package com.example.finalapp
 
+import android.content.Context
 import android.os.Bundle
 import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.SeekBarPreference
 
 
 class SettingsFragment : PreferenceFragmentCompat() {
+
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.settings, rootKey)
+
     }
 }
