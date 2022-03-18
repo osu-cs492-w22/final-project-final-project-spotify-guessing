151d4f9bdb7c244bf43b2315e5a4f95b5578ae18
Eduardo-Gonz <erugonz249@gmail.com>
2022-03-18 00:03:16 -0700

    Clean up gameplay and add end of game message

---
+++ app/src/main/java/com/example/finalapp/DialogFragment.kt
@@ -0,0 +1,43 @@
+package com.example.finalapp
+
+import android.app.Dialog
+import android.content.DialogInterface
+import android.content.Intent
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
+import androidx.fragment.app.DialogFragment
+
+class EndGameDialogFragment(private val score: Int) : DialogFragment() {
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        return activity?.let {
+            // Use the Builder class for convenient dialog construction
+            val builder = AlertDialog.Builder(it)
+            val inflater = requireActivity().layoutInflater
+            val view = inflater.inflate(R.layout.dialog_box, null)
+
+            view.findViewById<TextView>(R.id.dialog_score)?.text = score.toString()
+
+            // Inflate and set the layout for the dialog
+            // Pass null as the parent view because its going in the dialog layout
+            builder.setView(view)
+                // Add action buttons
+                .setPositiveButton(R.string.dialog_share,
+                    DialogInterface.OnClickListener { dialog, id ->
+                        val shareScore = "Score: $score"
+                        val intent = Intent().apply {
+                            action = Intent.ACTION_SEND
+                            putExtra(Intent.EXTRA_TEXT, shareScore)
+                            type = "text/plain"
+                        }
+                        startActivity(Intent.createChooser(intent, null))
+                    })
+                .setNegativeButton(R.string.dialog_close,
+                    DialogInterface.OnClickListener { dialog, id ->
+                        getDialog()?.cancel()
+                    })
+            builder.create()
+        } ?: throw IllegalStateException("Activity cannot be null")
+    }
+}
